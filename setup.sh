e58de6d86aa325619cde6082a377e3378ca4665b
Shilpa Sequeira <mail@shilpasequeira.com>
2016-02-17 11:14:30 -0800

    Modified setup

--- setup.sh
+++ setup.sh
@@ -6,13 +6,13 @@ set -eo pipefail
 
 echo "--- Clone student assignment branch"
 
-git clone -b $BRANCH_NAME $COURSE_REPO
+git clone -b $BRANCH_NAME $COURSE_REPO .
 
 echo "--- Clone graded tests"
 
 cd src/test
 
-git clone -b $BRANCH_NAME $GRADING_TESTS_REPO
+git clone -b $BRANCH_NAME $GRADING_TESTS_REPO .
 
 cd ../..
 
