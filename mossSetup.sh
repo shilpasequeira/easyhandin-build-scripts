101456d6c876937d72ef60e612a343cef60e53e7
nakisa6 <nakisa.abbasi@gmail.com>
2016-03-13 18:15:19 -0700

    adds sha for repos

--- mossSetup.sh
+++ mossSetup.sh
@@ -14,15 +14,33 @@ cd assignment
 echo "--- Iterate over student repos"
 IFS=",";
 
-repoArray=($STUDENTS_REPOS)
+declare -a repoArray
+declare -a SHAarray
 
+repoArray=($STUDENTS_REPOS)
+SHAarray=($SHA1_OF_COMMITS)
+
+#repors are:
 echo ${repoArray[0]}
 echo ${repoArray[1]}
+#shas are :
+echo ${SHAarray[0]}
+echo ${SHAarray[1]}
 
-for element in $repoArray
+#length of repos is:
+length=${#repoArray[@]}
+echo "length1 is: "$length
+#length of shas is:
+length2=${#SHAarray[@]}
+echo "length2 is: "$length2
+
+for (( i=0; i<length; i++ ))
 do
-   echo $BRANCH_NAME $element
-   git clone -b $BRANCH_NAME $element
+    echo $BRANCH_NAME ${repoArray[i]}
+    git clone -b $BRANCH_NAME ${repoArray[i]}
+    # reset this repository's master branch to the commit of interest
+    git reset --hard ${SHAarray[i]}
+    #git checkout HEAD -- <path>
 done
 
 cd ..
