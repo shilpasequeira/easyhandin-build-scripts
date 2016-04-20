9a1abb8c9a51d9565226c201e28fbb5a9fc2fcce
Shilpa Sequeira <mail@shilpasequeira.com>
2016-04-20 12:05:11 -0700

    Fixes resetting of sha for assignment

--- setup.sh
+++ setup.sh
@@ -8,20 +8,15 @@ rm -rf assignment
 echo "--- Clone student assignment branch"
 git clone -b $BRANCH_NAME $COURSE_REPO assignment
 
-cd assignment/src
-
-echo "-- Delete student unit tests"
-rm -rf test
+echo "--- Resetting to SHA within deadline"
+cd assignment
+git reset --hard $SHA
 
 echo "--- Clone graded tests"
+cd src
 git clone -b $BRANCH_NAME $GRADING_TESTS_REPO test
 
-cd test
-git reset --hard $SHA
-cd ../../..
+cd ../..
 
 echo "+++ Create container and run test"
 docker run -u=$UID -v ${BUILDKITE_BUILD_CHECKOUT_PATH}/assignment/:${BUILDKITE_BUILD_CHECKOUT_PATH}/assignment/ -w ${BUILDKITE_BUILD_CHECKOUT_PATH}/assignment/ --rm -i -t maven mvn clean test
-
-
-
